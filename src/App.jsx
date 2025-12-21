419ea5a818b005a7b3ded650a8c3004ebdb8bb56
MohammadSajidCS27 <mdsajid34887@gmail.com>
2025-12-21 19:10:48 +0530

    Update README.md

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ButtonSection from './components/ButtonSection'
 import AppHeader from './components/AppHeader'
 import { useState } from 'react'
 function App() {
-    let buttons=['C','1','2','+','3','4','-','5','6','*','7','8','.','9','0','=']
+    let buttons=['C','1','2','+','3','4','-','5','6','*','7','8','.','9','0','/','=']
     const [display,setDisplay]=useState('')
 
     const buttonHandling=(event)=>{
